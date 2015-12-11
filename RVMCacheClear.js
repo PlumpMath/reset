7cd87d48287f3c84c41707b095b90b854ce169e4
Dwayne Jones <dwayne@openfin.co>
2015-12-11 16:10:34 -0500

    Updates

--- RVMCacheClear.js
+++ RVMCacheClear.js
@@ -4,16 +4,26 @@ var watch = require('watch'),
     exec = require('child_process').exec,
     asarBuildFolder = 'Z:\\Projects\\runtime-core',
     version = process.argv[2],
+    launchConfig = process.argv[3],
     completionTrigger = asarBuildFolder + '\\staging\\core\\package.json', //currently the last thing in the grunt build-dev to get moved
     artifact = asarBuildFolder + '\\staging\\core',
     ofApp = 'C:\\Users\\Dwaynekj\\AppData\\Local\\OpenFin\\runtime\\' + version + '\\OpenFin\\resources\\default_app',
+    appASAR = 'C:\\Users\\Dwaynekj\\AppData\\Local\\OpenFin\\runtime\\' + version + '\\OpenFin\\resources\\app.asar',
     ofCache = 'C:\\Users\\Dwaynekj\\AppData\\Local\\OpenFin\\cache',
-    launcher = 'C:\\Users\\Dwaynekj\\AppData\\Local\\OpenFin\\runtime\\' + version + '\\OpenFin\\openfin.exe';
+    killProcesses = 'taskkill /f /im "openfin.exe" /T',
+    launcher = 'C:\\Users\\Dwaynekj\\AppData\\Local\\OpenFin\\runtime\\' + version + '\\OpenFin\\openfin.exe',
+    launchCommand = launcher + ' -l -c --startup-url="' +launchConfig+ '"';
 
 //watch.watchTree(asarBuildFolder, function() {
     if (fs.existsSync(completionTrigger)) {
         if (fs.existsSync(ofApp)) {
           wrench.rmdirSyncRecursive(ofApp, false);
+          console.log("deleted old default_app");
+        }
+
+        if (fs.existsSync(appASAR)) {
+          wrench.rmdirSyncRecursive(appASAR, false);
+          console.log("deleted app.asar");
         }
         wrench.copyDirSyncRecursive(artifact, ofApp, {
             forceDelete: true, // Whether to overwrite existing directory or not
@@ -26,12 +36,22 @@ var watch = require('watch'),
             // include: regexpOrFunction, // An include filter (either a regexp or a function)
             // exclude: regexpOrFunction // An exclude filter (either a regexp or a function)
         });
-        console.log('changed!');
-        exec('taskkill /f /im "OpenFinRVM.exe" /T', function(error, stdout, stderr) {
-
+        console.log('default_app updated!');
+        exec(killProcesses, function(error, stdout, stderr) {
+            console.log(killProcesses);
+            console.log(stdout);
+            console.log(stderr);
             if (fs.existsSync(ofCache)) {
-
                 wrench.rmdirSyncRecursive(ofCache, false);
+                console.log("deleted cache");
+            }
+
+            if (launchConfig){
+              console.log(launchCommand);
+              exec(launchCommand, function(error, stdout, stderr) {
+                  console.log(stdout);
+                  console.log(stderr);
+              });
             }
         });
     }
